4a598c694f3eecd356c359a2ef1e42bee5ba767b
Sun Kit Tsui <sunkittsui@gmail.com>
2024-12-20 14:27:09 -0800

    squash me: indentation fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,41 +2,41 @@ import { cn, getPreferedTheme, setPreferedTheme } from "@/utils";
 import { useEffect, useState } from "react";
 
 function App() {
-	useEffect(() => {
-		setPreferedTheme(getPreferedTheme());
-	}, []);
+  useEffect(() => {
+    setPreferedTheme(getPreferedTheme());
+  }, []);
 
-	const [isLight, setIsLight] = useState<boolean>(
-		getPreferedTheme() === "light",
-	);
+  const [isLight, setIsLight] = useState<boolean>(
+    getPreferedTheme() === "light",
+  );
 
-	const handleToggleDarkMode = () => {
-		const newTheme = getPreferedTheme() === "light" ? "dark" : "light";
-		setPreferedTheme(newTheme);
+  const handleToggleDarkMode = () => {
+    const newTheme = getPreferedTheme() === "light" ? "dark" : "light";
+    setPreferedTheme(newTheme);
 
-		setIsLight(newTheme === "light");
-	};
+    setIsLight(newTheme === "light");
+  };
 
-	return (
-		<main
-			className={cn(
-				"h-[100vh] w-[100vw] flex flex-col items-center justify-start p-5 gap-4 bg-white dark:bg-black",
-			)}
-		>
-			<h1 className="text-blue-700 dark:text-white font-bold text-5xl">
-				Hello, Filete!
-			</h1>
-			<button
-				className={cn("px-5 py-2 rounded bg-blue-700 text-white", {
-					"bg-blue-300": !isLight,
-				})}
-				onClick={handleToggleDarkMode}
-				type="button"
-			>
-				{isLight ? "Dark" : "Light"}
-			</button>
-		</main>
-	);
+  return (
+    <main
+      className={cn(
+        "h-[100vh] w-[100vw] flex flex-col items-center justify-start p-5 gap-4 bg-white dark:bg-black",
+      )}
+    >
+      <h1 className="text-blue-700 dark:text-white font-bold text-5xl">
+        Hello, Filete!
+      </h1>
+      <button
+        className={cn("px-5 py-2 rounded bg-blue-700 text-white", {
+          "bg-blue-300": !isLight,
+        })}
+        onClick={handleToggleDarkMode}
+        type="button"
+      >
+        {isLight ? "Dark" : "Light"}
+      </button>
+    </main>
+  );
 }
 
 export default App;
